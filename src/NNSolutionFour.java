f23ad78b124eab79f226ec08fa7e25625457a4d6
Borbola Martin <borbolam14@gmail.com>
2017-11-05 20:03:32 +0100

    Initial

---
+++ src/NNSolutionFour.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Tintin on 2017. 11. 05..
+ * Random thought
+ */
+
+// tanítás -> txt (neurális hálóról)
+// N bemenetek száma
+// M kimenetek száma
+// L rejtett rétegek száma
+
+public class NNSolutionFour {
+}
